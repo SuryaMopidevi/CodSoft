52144d612a6c9e022dc25b46550e10ad1794c48e
MOPIDEVI PAVITRASURYA <94208004+SuryaMopidevi@users.noreply.github.com>
2023-11-06 09:53:24 +0530

    commit

--- calc.py
+++ calc.py
@@ -39,23 +39,13 @@ def division(num1,num2):
     except ValueError:
         print("Invalid input. Please enter valid numeric values.")
 
-# modulo operation
-def modulo(num1,num2):
-    try :
-        num1 = complex(num1)
-        num2 = complex(num2)
-        result = max(num1.real,num2.real) % min(num1.real,num2.real)
-        print(f"{max(num1.real,num2.real)} % {min(num1.real,num2.real)} : {result.real}")
-    except ValueError:
-        print("Invalid input. Please enter valid numeric values.")
-
 
 # taking input numbers 
 num1 = input("Enter the first number: ")
 num2 = input("Enter the second number: ")
 
 # taking input arithematic operator
-operator=input("Enter Operator(+,-,*,//,%) : ")
+operator=input("Enter Operator(+,-,*,//) : ")
 
 # checking an operator for performing operation
 if operator=='+' :
@@ -64,8 +54,6 @@ elif operator == '-' :
     subtract(num1,num2)
 elif operator == '*' :
     product(num1,num2)
-elif operator == '%' :
-    modulo(num1,num2)
 elif operator == '//' :
     division(num1,num2)
 else :
